ce70936d554fb2d61bcccd3e9f0821f596b22cd0
sometime1992 <sometime1992@naver.com>
2024-11-22 17:50:34 +0900

    20241122 cjh

--- project-rrs/src/main/java/com/korit/projectrrs/dto/todo/request/TodoUpdateRequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.korit.projectrrs.dto.todo.request;
-
-import java.util.Date;
-
-public class TodoUpdateRequestDto {
-    String todoPreparationContent;
-}
--- project-rrs/src/main/java/com/korit/projectrrs/dto/todo/response/TodoUpdateResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.korit.projectrrs.dto.todo.response;
-
-import java.util.Date;
-
-public class TodoUpdateResponseDto {
-    Long todoId;
-    String todoPreparationContent;
-}
