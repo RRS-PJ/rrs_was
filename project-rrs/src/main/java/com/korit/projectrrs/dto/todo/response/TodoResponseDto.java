e562ad58dcdc5a13e7ff7e4adb75fb689093c7ef
kim dahye <makamong0000@gmail.com>
2024-12-26 17:30:05 +0900

    241226 kdh

--- project-rrs/src/main/java/com/korit/projectrrs/dto/todo/response/TodoResponseDto.java
+++ project-rrs/src/main/java/com/korit/projectrrs/dto/todo/response/TodoResponseDto.java
@@ -7,11 +7,13 @@ import java.time.LocalDate;
 
 @Data
 public class TodoResponseDto {
+    private Long todoId;
     private String todoPreparationContent;
     private LocalDate todoCreateAt;
     private Character todoStatus;
 
     public TodoResponseDto(Todo todo){
+        this.todoId = todo.getTodoId();
         this.todoPreparationContent = todo.getTodoPreparationContent();
         this.todoCreateAt = todo.getTodoCreateAt();
         this.todoStatus = todo.getTodoStatus();
