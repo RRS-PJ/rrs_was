d08be4cc6a8a8bf91239d63227aec692586c84fb
sometime1992 <sometime1992@naver.com>
2025-01-04 21:28:09 +0900

    20250104 cjh

--- project-rrs/src/main/java/com/korit/projectrrs/repositoiry/CommunityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.korit.projectrrs.repositoiry;
-
-import com.korit.projectrrs.entity.Community;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommunityRepository extends JpaRepository<Community, Long> {
-}
