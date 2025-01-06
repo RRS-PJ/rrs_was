11b55b42fa19ca297be614fb78d0adce879ae30a
sometime1992 <sometime1992@naver.com>
2025-01-06 09:18:18 +0900

    20250106 cjh

--- project-rrs/src/main/java/com/korit/projectrrs/dto/community/request/CommunityCreateRequestDto.java
+++ project-rrs/src/main/java/com/korit/projectrrs/dto/community/request/CommunityCreateRequestDto.java
@@ -1,6 +1,5 @@
 package com.korit.projectrrs.dto.community.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
