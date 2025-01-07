4a6a00ee6f9de25a69fbaae653f820937fad0389
sometime1992 <sometime1992@naver.com>
2025-01-07 17:25:59 +0900

    20250107 cjh

--- project-rrs/src/main/java/com/korit/projectrrs/dto/healthrecord/response/HealthRecordAllResponseDto.java
+++ project-rrs/src/main/java/com/korit/projectrrs/dto/healthrecord/response/HealthRecordAllResponseDto.java
@@ -1,10 +1,15 @@
 package com.korit.projectrrs.dto.healthrecord.response;
 
 import com.korit.projectrrs.entity.HealthRecord;
+import com.korit.projectrrs.entity.HealthRecordAttachment;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -16,6 +21,7 @@ public class HealthRecordAllResponseDto {
     private String abnormalSymptoms;
     private String memo;
     private LocalDate createdAt;
+    private List<String> attachments; // 첨부파일 리스트 추가
 
     public HealthRecordAllResponseDto(HealthRecord healthRecord) {
         this.healthRecordId = healthRecord.getHealthRecordId();
@@ -25,5 +31,12 @@ public class HealthRecordAllResponseDto {
         this.abnormalSymptoms = healthRecord.getAbnormalSymptoms();
         this.memo = healthRecord.getMemo();
         this.createdAt = healthRecord.getCreatedAt();
+
+        // Optional을 사용하여 첨부파일 경로 리스트를 안전하게 처리
+        this.attachments = Optional.ofNullable(healthRecord.getAttachments())
+                .orElseGet(Collections::emptyList) // null일 경우 빈 리스트 반환
+                .stream()
+                .map(HealthRecordAttachment::getHealthRecordAttachmentFile) // 파일 경로를 리스트로 변환
+                .collect(Collectors.toList());
     }
 }
