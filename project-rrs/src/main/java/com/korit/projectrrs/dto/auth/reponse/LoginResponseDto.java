f47766e9d57bab6002812d63bac3eacef957c850
kim dahye <makamong0000@gmail.com>
2025-01-10 15:24:09 +0900

    250110kdh

--- project-rrs/src/main/java/com/korit/projectrrs/dto/auth/reponse/LoginResponseDto.java
+++ project-rrs/src/main/java/com/korit/projectrrs/dto/auth/reponse/LoginResponseDto.java
@@ -1,13 +1,27 @@
 package com.korit.projectrrs.dto.auth.reponse;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.korit.projectrrs.entity.User;
+import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class LoginResponseDto {
-    private User user;
+    private Long userId;
+    private String name;
+    private String username;
+    private String password;
+    private String nickname;
+    private String phone;
+    private String address;
+    private String addressDetail;
+    private String email;
+    private String profileImageUrl;
+    private String roles;
+    private String providerIntroduction;
+
     private String token;
     private int exprTime;
 }
