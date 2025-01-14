71e8d2526ddf970c514150a373640a9cbd1e188a
sueun0812 <plok9408@naver.com>
2025-01-14 23:35:48 +0900

    250114 jse

--- project-rrs/src/main/java/com/korit/projectrrs/dto/provision/responseDto/ProvisionResponseDto.java
+++ project-rrs/src/main/java/com/korit/projectrrs/dto/provision/responseDto/ProvisionResponseDto.java
@@ -13,17 +13,47 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProvisionResponseDto {
-        private Long reservationId;
-        private LocalDate reservationStartDate;
-        private LocalDate reservationEndDate;
-        private List<Pet> pets;
-        private ReservationStatus reservationStatus;
+    private Long reservationId;
+    private LocalDate reservationStartDate;
+    private LocalDate reservationEndDate;
 
-        public ProvisionResponseDto(Reservation reservation) {
-            this.reservationId = reservation.getReservationId();
-            this.reservationStartDate = reservation.getReservationStartDate();
-            this.reservationEndDate = reservation.getReservationEndDate();
-            this.pets = reservation.getUser().getPet();
-            this.reservationStatus = reservation.getReservationStatus();
+    private Long petId;
+    private String petName;
+    private Character petGender;
+    private String petBirthDate;
+    private Integer petWeight;
+    private String petImageUrl;
+    private String petAddInfo;
+    private Character petNeutralityYn;
+
+    private Long userId;
+    private String nickName;
+    private String address;
+    private String phone;
+
+    private String reservationMemo;
+    private ReservationStatus reservationStatus;
+
+    public ProvisionResponseDto(Reservation reservation) {
+        this.reservationId = reservation.getReservationId();
+        this.reservationStartDate = reservation.getReservationStartDate();
+        this.reservationEndDate = reservation.getReservationEndDate();
+
+        if (!reservation.getUser().getPet().isEmpty()) {
+            Pet pet = reservation.getUser().getPet().get(0); // 첫 번째 펫을 가져옴
+            this.petId = pet.getPetId();
+            this.petName = pet.getPetName();
+            this.petGender = pet.getPetGender();
+            this.petBirthDate = pet.getPetBirthDate();
+            this.petWeight = pet.getPetWeight();
+            this.petImageUrl = pet.getPetImageUrl();
+            this.petAddInfo = pet.getPetAddInfo();
+            this.petNeutralityYn = pet.getPetNeutralityYn();
         }
+        this.nickName = reservation.getUser().getNickname();
+        this.address = reservation.getUser().getAddress();
+        this.phone = reservation.getUser().getPhone();
+        this.reservationMemo = reservation.getReservationMemo();
+        this.reservationStatus = reservation.getReservationStatus();
+    }
 }
